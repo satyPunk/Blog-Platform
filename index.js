96d7dec29adecfd203fe40d2d6e351c9215180d4
satyPunk <satyamhere8@gmail.com>
2025-02-01 09:00:20 +0530

    INITIAL COMMIT

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+//body parser
+//include ft,h
+//main.css
+
+const app =express()
+const port =3000
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"))
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
+})
+app.get("/write",(req,res)=>{
+    res.render("write.ejs")
+})
+app.get("/read",(req,res)=>{
+    res.render("read.ejs")
+})
+
+app.listen(port,()=>{
+    console.log("port open")
+})
